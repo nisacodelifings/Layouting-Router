f86891bbec0791028e2b4fb31b44a7dd05394760
Annisa Puspitasari <nisa.codelifings@gmail.com>
2020-10-03 20:51:22 +0700

    Initial commit

---
+++ src/SPA.js
@@ -0,0 +1,72 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import "./style.css";
+
+export default function SPAku() {
+  return (
+    <Router>
+      <div class="container">
+
+        <div class="header">nisa's blog</div>
+
+        <div class="menu">
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <li>
+                <Link to="/dashboard">Dashboard</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+        <div class="content">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/dashboard">
+              <Dashboard />
+            </Route>
+          </Switch>
+        </div>
+
+        <div class="footer">created with love</div>
+
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+    </div>
+  );
+}
